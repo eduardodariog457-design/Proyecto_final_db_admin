2fafa75d6a4c896546f2abc71b95a53a427ddeaa
jordanavalos09092004 <148576904+jordanavalos09092004@users.noreply.github.com>
2026-08-21 14:42:48 -0600

    Grant usage on sequences to captura role
    Added grants for usage on sequences to the captura role.

--- sql/03_usuarios_permisos.sql
+++ sql/03_usuarios_permisos.sql
@@ -4,6 +4,8 @@ create role captura;
 grant usage on schema streaming to consulta;
 grant usage on schema streaming to captura;
 
+grant usage on all sequences in schema streaming to captura;
+
 grant select on all tables in schema streaming to consulta;
 grant insert on all tables in schema streaming to captura;
 
@@ -12,10 +14,12 @@ alter default privileges in schema streaming
 alter default privileges in schema streaming
     grant insert on tables to captura;
 
+alter default privileges in schema streaming
+    grant usage on sequences to captura;
+
 create user usr_consulta with password 'consulta_123';
 create user usr_captura  with password 'captura_123';
 
 grant consulta to usr_consulta;
 grant captura  to usr_captura;
-
 
